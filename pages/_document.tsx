778bdc151ea0d70fe995e0223ca6c4ce2a2e064e
Omar <omargeerman@gmail.com>
2021-03-16 10:53:53 +0100

    gead warning fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <title>{title}</title>
           <meta name="description" content={description} />
           <meta property="og:type" content="website" />
           <meta property="og:title" content={title} />
